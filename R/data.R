9e1f5c31a3b204ea8c84e6aa29cf612fd25c14d3
karirogg <karirogg@gmail.com>
2021-08-19 15:31:12 +0000

    added documentation to dat_KP

--- R/data.R
+++ R/data.R
@@ -10,3 +10,16 @@
 #' }
 #' @source <https://github.com/karirogg/ScotsPine>
 "dat_TA"
+
+#' @title Sapwood statistics from Sweden
+#'
+#' @description A dataset of sapwood statistics from historical samples from Kälksjön, Sweden
+#'
+#' @format A data frame with 59 rows and 4 variables:
+#' \describe{
+#'   \item{H}{Number of heartwood rings}
+#'   \item{S}{Number of sapwood rings}
+#'   \item{W}{Mean tree ring width of heartwood}
+#' }
+#' @source <https://github.com/karirogg/ScotsPine>
+"dat_KP"
